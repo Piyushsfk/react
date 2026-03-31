fc88c6472642f5b882ebc294f8e5a17f16844102
Piyushsfk <piyushcyb@gmail.com>
2026-03-31 18:00:05 +0530

    react cards and object changing

--- 03tailwind/src/App.jsx
+++ 03tailwind/src/App.jsx
@@ -3,49 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card7 from './components/card2'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
+  let myObj={
+    username:"Piyush",
+    age:"21"
+  }
 
   return (
     <>
     <h1 className='bg-green-400 text-black p-4 rounded-xl'>Frontend</h1>
-    import React from "react";
-
-export default function Card10() {
-  return (
-    <div
-      className="flex flex-col rounded-xl  p-4"
-      style={{
-        border: "0.88px solid",
-
-        backdropFilter: "saturate(180%) blur(14px)",
-        background: " #ffffff0d",
-      }}
-    >
-      <div>
-        <img
-          src="https://res.cloudinary.com/ddcg0rzlo/image/upload/v1652470298/9StaF0UBJfih_df0248.gif"
-          alt="nft-gif"
-          width="350"
-          height="350"
-          className="rounded-xl"
-        />
-      </div>
-      <div className="flex flex-col  rounded-b-xl py-4 ">
-        <div className="flex justify-between">
-          <h1 className="font-RubikBold ">Bored ape nft accidental</h1>
-          <h1 className="font-bold font-RubikBold">Price</h1>
-        </div>
-        <div className="flex  justify-between font-mono">
-          <p>#345</p>
-          <p>0.01</p>
-        </div>
-      </div>
-    </div>
-  );
-}
-
+    <Card7 username="Nukes" okong={myObj}/>
+    <Card   okong={myObj}/>
     </>
   )
 }
